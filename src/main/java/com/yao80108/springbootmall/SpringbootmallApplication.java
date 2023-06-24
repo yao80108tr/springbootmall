a53be4a0209b4dc5b5b21a9d91b9cebdbbf59d33
r71700win102 <yao80108tr@gmail.com>
2023-06-24 18:59:51 +0800

    初始化 Spring Boot Mall

---
+++ src/main/java/com/yao80108/springbootmall/SpringbootmallApplication.java
@@ -0,0 +1,13 @@
+package com.yao80108.springbootmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootmallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yao80108/springbootmall/SpringbootmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yao80108.springbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootmallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
