bcbd3f56a8377102e9377d8ca43f7bd5d4fca3ef
r71700win102 <yao80108tr@gmail.com>
2023-07-01 17:20:58 +0800

    商品功能 - 提煉程式

--- src/main/java/com/yao80108/springbootmall/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/yao80108/springbootmall/dao/impl/ProductDaoImpl.java
@@ -29,14 +29,7 @@ public class ProductDaoImpl implements ProductDao {
                 "      WHERE 1 = 1";
         Map<String, Object> map = new HashMap<>();
         // 查詢條件
-        if(productQueryParams.getCategory() != null){
-            sql = sql + " AND category = :category";
-            map.put("category", productQueryParams.getCategory().name());
-        }
-        if(productQueryParams.getSearch() != null){
-            sql = sql + " AND product_name LIKE :search";
-            map.put("search", "%" + productQueryParams.getSearch() + "%");
-        }
+        sql = addFilteringSql(sql, map, productQueryParams);
         // 排序
         sql = sql + " ORDER BY " + productQueryParams.getOrderBy() + " " + productQueryParams.getSort();
         // 分頁
@@ -128,6 +121,13 @@ public class ProductDaoImpl implements ProductDao {
                 "      WHERE 1 = 1";
         Map<String, Object> map = new HashMap<>();
         // 查詢條件
+        sql = addFilteringSql(sql, map, productQueryParams);
+        Integer total = namedParameterJdbcTemplate.queryForObject(sql, map, Integer.class);
+        return (total);
+    }
+
+    private String addFilteringSql(String sql, Map<String, Object> map, ProductQueryParams productQueryParams){
+        // 查詢條件
         if (productQueryParams.getCategory() != null) {
             sql = sql + " AND category = :category";
             map.put("category", productQueryParams.getCategory().name());
@@ -136,7 +136,6 @@ public class ProductDaoImpl implements ProductDao {
             sql = sql + " AND product_name LIKE :search";
             map.put("search", "%" + productQueryParams.getSearch() + "%");
         }
-        Integer total = namedParameterJdbcTemplate.queryForObject(sql, map, Integer.class);
-        return (total);
+        return sql;
     }
 }
